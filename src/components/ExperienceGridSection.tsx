76324079fbb9a2f160a62e0b4d716597695f8ff8
Niltinho77 <nilton.cn1992@aluno.ifsc.edu.br>
2025-12-18 20:43:08 -0300

    teste apagando bc

--- src/components/ExperienceGridSection.tsx
+++ src/components/ExperienceGridSection.tsx
@@ -68,8 +68,8 @@ const tiles: Tile[] = [
     subtitle: "4 pessoas. Um objetivo.",
     location: "Inscrições abertas",
     image: "/images/equipes.png",
-    disabled: false,  
-    href: "/checkout?modality=equipes",
+    disabled: true,  
+    //href: "/checkout?modality=equipes",
 
   },
   {
